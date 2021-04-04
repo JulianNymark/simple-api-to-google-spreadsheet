ae95274a298b0d7d60de929cfb9f5bd4fb545c5d
Julian Nymark <j@juliannymark.com>
2021-04-04 16:41:47 +0200

    refactor: enhance structure for future feature addition!

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,7 @@
+import { args } from "./arguments";
 import { appendData, getData } from "./spreadsheet";
 
-let args = process.argv.slice(2);
-
-if (args.length > 2 || args.length < 1) {
-  console.error(`
-  USAGE:
-    npm start <spreadsheet_id> [range='A:B']
-  `)
-  throw new Error('incorrect parameters');
-}
-
-const spreadsheetId = args[0]
-const range = args[1] ?? 'A:B'
-
-console.log(`spreadsheet: ${spreadsheetId}`);
-console.log(`range: ${range}`);
+const {spreadsheetId, range} = args();
 
 (async () => {
   let data = await getData(spreadsheetId, range);
