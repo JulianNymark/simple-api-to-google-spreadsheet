45f3fa56524d2e781edaf5ee835a12208300213b
Julian Nymark <j@juliannymark.com>
2021-04-04 14:29:06 +0200

    append data [stable working commit]

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-// import fs from "fs";
-// import readline from "readline";
-import { google } from "googleapis";
+// import fs from 'fs';
+// import readline from 'readline';
+import { google } from 'googleapis';
 
-import privatekey from "../service-account-credentials.json";
+import privatekey from '../service-account-credentials.json';
 
 // configure a JWT auth client
 let jwtClient = new google.auth.JWT(
   privatekey.client_email,
   undefined,
   privatekey.private_key,
-  ["https://www.googleapis.com/auth/spreadsheets"]
+  ['https://www.googleapis.com/auth/spreadsheets']
 );
 
 jwtClient.authorize(function (err, tokens) {
@@ -17,30 +17,30 @@ jwtClient.authorize(function (err, tokens) {
     console.error(err);
     return;
   } else {
-    console.log("Successfully connected!");
+    console.log('Successfully connected!');
   }
 });
 
-const sheets = google.sheets("v4");
+const sheets = google.sheets('v4');
 
 const getData = async (range: string) => {
   const promise: Promise<any[][]> = new Promise((resolve, reject) => {
     sheets.spreadsheets.values.get(
       {
         auth: jwtClient,
-        spreadsheetId: "1qcmxYjLvM4dXmT4aKg3Lkc_BJdUhItQrvR1tWXmIbpA",
+        spreadsheetId: '1qcmxYjLvM4dXmT4aKg3Lkc_BJdUhItQrvR1tWXmIbpA',
         range,
       },
       (err, res) => {
-        if (err) return reject("The API returned an error: " + err);
+        if (err) return reject('The API returned an error: ' + err);
 
-        if (!res) return reject("Bad response from google API: " + res);
+        if (!res) return reject('Bad response from google API: ' + res);
 
         const rows = res.data.values;
         if (rows && rows.length) {
           return resolve(rows);
         } else {
-          return reject("No data found.");
+          return reject('No data found.');
         }
       }
     );
@@ -48,7 +48,35 @@ const getData = async (range: string) => {
   return promise;
 };
 
+const appendData = async (range: string, rowsData: any[][]) => {
+  const promise: Promise<void> = new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.append(
+      {
+        auth: jwtClient,
+        spreadsheetId: '1qcmxYjLvM4dXmT4aKg3Lkc_BJdUhItQrvR1tWXmIbpA',
+        range,
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        requestBody: {
+          values: rowsData,
+        },
+      },
+      (err, res) => {
+        if (err) return reject('The API returned an error: ' + err);
+
+        return resolve();
+      }
+    );
+  });
+  return promise;
+};
+
 (async () => {
-  const data = await getData("A:B");
-  console.log(data[0][0]);
+  let data = await getData('A:B');
+  console.log('data:', data);
+
+  await appendData('A:B', [['hello!', 42, 4.2, 'true', true]]);
+
+  data = await getData('A:B');
+  console.log('data:', data);
 })();
